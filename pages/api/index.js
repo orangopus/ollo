c4653f76c7669f81607e85c1480b0a031f09581d
xpbsh <45247477+xpbsh@users.noreply.github.com>
2021-02-17 20:43:46 +0000

    Delete index.js

--- pages/api/index.js
+++
@@ -1,9 +0,0 @@
-import config from "../../config.json";
-
-export default (req, res) => {
-  res.statusCode = 200;
-
-  res.end(
-    JSON.stringify({ message: "Welcome to the API " + config.user.username })
-  );
-};
